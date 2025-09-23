d8d51f8a9d180932a3d2cbed3a2cf787d9bb3d33
Robert Hargreaves <kiyouta@gmail.com>
2025-09-23 22:54:21 +0100

    fix: definitely a sine wave now
    or is it... sounds like something is off?

--- main.cpp
+++ main.cpp
@@ -2,16 +2,32 @@
 #include <iostream>
 
 constexpr unsigned int SampleRate = 44100;
-constexpr unsigned int BufferSize = 1024;
+constexpr unsigned int BufferSize = 256;
 constexpr unsigned int Channels = 2;
+constexpr float Gain = 0.2;
+
+struct SineState {
+    float phase = 0.0;
+    float phaseInc = 2.0 * M_PI * 440.0 / static_cast<float>(SampleRate);
+};
 
 int sineWave(void *outputBuffer, void *inputBuffer,
              unsigned int nBufferFrames, double streamTime,
              RtAudioStreamStatus status, void *userData) {
 
+    auto state = static_cast<SineState *>(userData);
     auto buffer = static_cast<float *>(outputBuffer);
     for (int i = 0; i < nBufferFrames; i++) {
-        buffer[i] = sin(2 * M_PI * 440 * i / SampleRate);
+
+        float s = std::sin(state->phase) * Gain;
+        state->phase += state->phaseInc;
+        if (state->phase >= 2.0 * M_PI) {
+            state->phase -= 2.0 * M_PI;
+        }
+
+        for (int c = 0; c < Channels; c++) {
+            *buffer++ = s;
+        }
     }
     return 0;
 }
@@ -30,9 +46,11 @@ int main() {
         .firstChannel = 0};
     unsigned int bufferSize = BufferSize;
 
+    SineState state;
+
     auto res = audio.openStream(&outParams,
         nullptr, RTAUDIO_FLOAT32,
-                                SampleRate, &bufferSize, &sineWave);
+                                SampleRate, &bufferSize, &sineWave, &state);
     if (res != RTAUDIO_NO_ERROR) {
         std::cerr << "Failed to open stream: " << res << "\n";
         return EXIT_FAILURE;
