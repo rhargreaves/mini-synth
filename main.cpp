740b229c01fa3780d4def69d7825121900e785b5
Robert Hargreaves <kiyouta@gmail.com>
2025-09-23 22:35:39 +0100

    feat: play some horrible noise
    think the phasing needs to be set properly...

--- main.cpp
+++ main.cpp
@@ -1,12 +1,54 @@
-#include <iostream>
 #include <RtAudio.h>
+#include <iostream>
+
+constexpr unsigned int SampleRate = 44100;
+constexpr unsigned int BufferSize = 1024;
+constexpr unsigned int Channels = 2;
+
+int sineWave(void *outputBuffer, void *inputBuffer,
+             unsigned int nBufferFrames, double streamTime,
+             RtAudioStreamStatus status, void *userData) {
+
+    auto buffer = static_cast<float *>(outputBuffer);
+    for (int i = 0; i < nBufferFrames; i++) {
+        buffer[i] = sin(2 * M_PI * 440 * i / SampleRate);
+    }
+    return 0;
+}
 
 int main() {
     RtAudio audio;
 
-    std::cout << "Audio devices:" << std::endl;
-    for (auto& device : audio.getDeviceNames()) {
-        std::cout << device << std::endl;
+    if (audio.getDeviceCount() == 0) {
+        std::cerr << "No audio devices found.\n";
+        return EXIT_FAILURE;
+    }
+
+    RtAudio::StreamParameters outParams{
+        .deviceId = audio.getDefaultOutputDevice(),
+        .nChannels = Channels,
+        .firstChannel = 0};
+    unsigned int bufferSize = BufferSize;
+
+    auto res = audio.openStream(&outParams,
+        nullptr, RTAUDIO_FLOAT32,
+                                SampleRate, &bufferSize, &sineWave);
+    if (res != RTAUDIO_NO_ERROR) {
+        std::cerr << "Failed to open stream: " << res << "\n";
+        return EXIT_FAILURE;
+    }
+
+    if (audio.startStream() != RTAUDIO_NO_ERROR) {
+        std::cerr << "Failed to start stream.\n";
+        return EXIT_FAILURE;
+    }
+
+    char input;
+    std::cout << "Playing... press <enter> to quit.\n";
+    std::cin.get(input);
+
+    if (audio.isStreamRunning()) {
+        audio.stopStream();
     }
 
     return EXIT_SUCCESS;
