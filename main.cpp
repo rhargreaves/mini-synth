4dead9ab872d5dec86f20c4148fa9d7c4e2cd2ae
Robert Hargreaves <kiyouta@gmail.com>
2025-09-24 20:21:57 +0100

    refactor: minor simplifications

--- main.cpp
+++ main.cpp
@@ -6,29 +6,29 @@
 constexpr unsigned int SampleRate = 44100;
 constexpr unsigned int BufferSize = 256;
 constexpr unsigned int Channels = 2;
-constexpr float Gain = 0.2;
+constexpr float Gain = 0.2f;
+constexpr float PeriodRad = 2.0f * std::numbers::pi;
 
 struct SineState {
-    float phase = 0.0;
-    float phaseInc = 2.0 * std::numbers::pi * 440.0 / static_cast<float>(SampleRate);
+    float phase = 0.0f;
+    float freq = 440.0f;
 };
 
-int sineWave(void *outputBuffer, void *inputBuffer,
-             unsigned int nBufferFrames, double streamTime,
-             RtAudioStreamStatus status, void *userData) {
+int sineWave(void *outputBuffer, void *,
+             unsigned int nBufferFrames,
+             double, RtAudioStreamStatus, void *userData) {
 
-    auto state = static_cast<SineState *>(userData);
+    auto& state = *static_cast<SineState *>(userData);
     auto buffer = static_cast<float *>(outputBuffer);
     for (int i = 0; i < nBufferFrames; i++) {
-
-        float s = std::sin(state->phase) * Gain;
-        state->phase += state->phaseInc;
-        if (state->phase >= 2.0 * std::numbers::pi) {
-            state->phase -= 2.0 * std::numbers::pi;
+        float sample = std::sin(state.phase) * Gain;
+        state.phase += PeriodRad * state.freq / static_cast<float>(SampleRate);
+        if (state.phase >= PeriodRad) {
+            state.phase -= PeriodRad;
         }
 
         for (int c = 0; c < Channels; c++) {
-            *buffer++ = s;
+            *buffer++ = sample;
         }
     }
     return 0;
